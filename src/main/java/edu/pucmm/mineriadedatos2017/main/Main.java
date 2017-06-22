ac3b54b336eed62ba0b96254afb05670cefa5210
Gustavo Jose Henríquez Contreras <gustavojoseh@gmail.com>
2017-06-21 23:58:35 -0400

    Logger agregado y configurado.

--- src/main/java/edu/pucmm/mineriadedatos2017/main/Main.java
+++ src/main/java/edu/pucmm/mineriadedatos2017/main/Main.java
@@ -4,18 +4,28 @@ import edu.pucmm.mineriadedatos2017.util.FXMLUtil;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 /**
  * Created by jeanl on 21 jun 2017.
  */
 public class Main extends Application {
+    private static final Logger logger = LogManager.getLogger();
 
     private final String TITULO = "WekaFX";
     private final int MIN_WIDTH = 900;
     private final int MIN_HEIGHT = 600;
 
+    public static void main(String[] args) {
+        logger.info("Iniciando programa.");
+        Application.launch(Main.class, args);
+        logger.info("Programa terminado.");
+    }
+
     @Override
     public void start(Stage primaryStage) throws Exception {
+        logger.info("Iniciando pantalla pricipal.");
         primaryStage = new Stage();
         primaryStage.setScene(new Scene(FXMLUtil.cargarFXML("/vista/PantallaPrincipal.fxml")));
         primaryStage.setTitle(TITULO);
@@ -23,9 +33,5 @@ public class Main extends Application {
         primaryStage.setMinWidth(MIN_WIDTH);
         primaryStage.setMinHeight(MIN_HEIGHT);
         primaryStage.show();
-    }
-
-    public static void main(String[] args) {
-        Application.launch(Main.class, args);
     }
 }
