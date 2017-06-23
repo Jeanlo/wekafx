075c69c385153df17f85fb10143a94611fec4a0e
Gustavo Jose Henríquez Contreras <gustavojoseh@gmail.com>
2017-06-23 14:50:00 -0400

    Vista principal y unica agregada y terminada. Enum agregado y clase controladora implementada.

--- src/main/java/edu/pucmm/mineriadedatos2017/controladora/PantallaPrincipalControladora.java
+++ src/main/java/edu/pucmm/mineriadedatos2017/controladora/PantallaPrincipalControladora.java
@@ -1,6 +1,15 @@
 package edu.pucmm.mineriadedatos2017.controladora;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import org.controlsfx.control.PrefixSelectionComboBox;
+import org.controlsfx.control.textfield.CustomTextField;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -9,6 +18,55 @@ import java.util.ResourceBundle;
  * Created by jeanl on 21 jun 2017.
  */
 public class PantallaPrincipalControladora implements Initializable {
+
+    @FXML
+    private StackPane stackPane;
+
+    @FXML
+    private BorderPane borderPane;
+
+    @FXML
+    private MenuBar menuBar;
+
+    @FXML
+    private Menu menu;
+
+    @FXML
+    private MenuItem menuItemAbrir;
+
+    @FXML
+    private MenuItem menuItemSalir;
+
+    @FXML
+    private VBox vBox;
+
+    @FXML
+    private ImageView imageView;
+
+    @FXML
+    private HBox hBox;
+
+    @FXML
+    private CustomTextField txtFjeldArchivo;
+
+    @FXML
+    private Button btnBuscarArchivo;
+
+    @FXML
+    private HBox hBox2;
+
+    @FXML
+    private PrefixSelectionComboBox<?> comboBoxSeleccioneAlgoritmo;
+
+    @FXML
+    private Button btnEjecutarAlgoritmo;
+
+    @FXML
+    private ScrollPane scrollPane;
+
+    @FXML
+    private TextArea txtArea;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
---
+++ src/main/java/edu/pucmm/mineriadedatos2017/enums/Algoritmo.java
@@ -0,0 +1,6 @@
+package edu.pucmm.mineriadedatos2017.enums;
+
+public enum Algoritmo {
+    NAIVE_BAYES,
+    J48
+}
