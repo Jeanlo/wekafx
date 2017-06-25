6fe34bda540d3e998baa71e3ca45057f2e1396be
Jeanlo <jeanlotejeda0101@hotmail.com>
2017-06-24 23:36:00 -0400

    Agregado añadir instancias nuevas.

--- src/main/java/edu/pucmm/mineriadedatos2017/algoritmos/AlgoritmoJ48.java
+++ src/main/java/edu/pucmm/mineriadedatos2017/algoritmos/AlgoritmoJ48.java
@@ -2,10 +2,13 @@ package edu.pucmm.mineriadedatos2017.algoritmos;
 
 import weka.classifiers.meta.FilteredClassifier;
 import weka.classifiers.trees.J48;
+import weka.core.DenseInstance;
+import weka.core.Instance;
 import weka.core.Instances;
 import weka.core.converters.ConverterUtils.DataSource;
 
 import java.io.File;
+import java.util.ArrayList;
 
 public class AlgoritmoJ48 {
     private File file;
@@ -27,16 +30,26 @@ public class AlgoritmoJ48 {
             j48.setUnpruned(true);
             FilteredClassifier fc = new FilteredClassifier();
             fc.setClassifier(j48);
+
+            Instance inst = new DenseInstance(data.numAttributes());
+            inst.setDataset(data);
+
+            String valoresSet = "sunny,cool,high,TRUE,no";
+            String [] valores = valoresSet.split(",");
+            for(int i = 0; i < data.numAttributes(); i++){
+                inst.setValue(i, valores[i]);
+            }
+            data.add(inst);
+
             fc.buildClassifier(data);
 
             for (int i = 0; i < data.numInstances(); i++) {
-                double pred = fc.classifyInstance(data.instance(0));
-                stringBuilder.append("No. " + data.instance(i).value(0) + "\n");
+                double pred = fc.classifyInstance(data.instance(i));
+                stringBuilder.append("No. " + (i+1) + "\n");
                 stringBuilder.append(data.instance(i).toString() + "\n");
                 stringBuilder.append("Valor predecido: " + data.classAttribute().value((int) pred) + "\n");
                 stringBuilder.append("Valor real: " + data.classAttribute().value((int) data.instance(i).classValue()) + "\n\n");
             }
-
 
         } catch (Exception e) {
             e.printStackTrace();
